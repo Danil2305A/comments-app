b5e0e43387d309e39bd2a7c6e3cb5cca03c5eb46
Akusko Danil Dmitrievich <akusko.dan@yandex.ru>
2026-01-10 23:00:50 +0900

    Init Spring Boot project

---
+++ server/src/test/java/com/comments/CommentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
